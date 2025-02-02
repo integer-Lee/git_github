3ce3cea7f9f06ddd20b71e189e744e2c92a6fc8d
JeongSu Lee <j100642@gmail.com>
2025-02-02 13:52:30 +0900

    fix

--- basic_syntax/1.basic_command.sh
+++ basic_syntax/1.basic_command.sh
@@ -1,2 +1,8 @@
 #git add . 은 모든 변경(추가)사항을 staging area로 이동하는 의미이다.
 git add .
+
+# git commit은 staging area의 변경사항을 확정짓고 commitID를 생성하는 것이다.
+git commit -m "메시지"
+
+#원격 repo에 main브랜치 변경사항을 업로드하는 것이다.
+git push origin main
